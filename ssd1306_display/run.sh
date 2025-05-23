bed0a0ab974a1e0eb2a5db8b0e48d77381d18835
Lucas Hyland <52391612+lucasjord@users.noreply.github.com>
2025-05-23 19:08:35 +1000

    Update run.sh

--- ssd1306_display/run.sh
+++ ssd1306_display/run.sh
@@ -3,11 +3,13 @@
 set -e
 
 echo "[INFO] Cloning SSD1306 repo..."
-git clone https://github.com/UCTRONICS/U6143_ssd1306.git /opt/ssd1306
+#git clone https://github.com/UCTRONICS/U6143_ssd1306.git /opt/ssd1306 # original 
+git clone https://github.com/lucasjord/U6143_ssd1306.git /opt/ssd1306  # new for this
 
 echo "[INFO] Compiling display.c..."
 cd /opt/ssd1306/C || exit 1
-gcc display.c -o display -lwiringPi
+#gcc display.c -o display -lwiringPi
+gcc display.c -o display -li2c
 
 echo "[INFO] Running the program..."
 ./display
